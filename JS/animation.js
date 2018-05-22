968eaa07d23fc07e7a33693b3429adb1bfa147ee
StarGale <zhenchix@hotmail.com>
2018-05-22 01:53:51 -0700

    wireframe

--- JS/animation.js
+++ JS/animation.js
@@ -1,5 +1,15 @@
 'user strict'
-// #login{
-    
-// };
 
+//// /ReDirect/////
+
+
+/////DELAY GIMMICK SIMULATE LOADING SCREEN///
+function delay() {
+    document.getElementById('submit')
+    var delay = setTimeout(reDirect, 1000);
+
+
+}
+function reDirect() {
+    window.location.href = "main.html";
+}
