9d0ef14c2bb9137d658fb75a82d6dc09998a75c9
Tara Johnson <tara@madface.com>
2018-05-24 20:39:26 -0700

    Login name added to local storage

--- JS/login.js
+++ JS/login.js
@@ -1,32 +1,46 @@
-'user strict'
+'use strict';
 
 //// /ReDirect/////
-
-
 /////DELAY GIMMICK SIMULATE LOADING SCREEN///
 function delay() {
-    document.getElementById('submit')
-    var delay = setTimeout(reDirect, 300);
-
-    
-
+    // document.getElementById('submit')
+    var delay = setTimeout(reDirect, 3000);
 }
+
 function reDirect() {
     window.location.href = 'main.html';
-    saveUserName();
+    // saveUserName();
 }
 
 //------------------------------
-//Save username to Local storage
-//------------------------------
-// var user = "code fellows";
-var userName = document.getElementById('userName').value;
-function saveUserName() {
-    // event.preventDefault();
-    // var userName=event.target.saveUserName.value;
-    localStorage.setItem('localUserName', JSON.stringify(userName));
+// Save username to local store
+var inputForm = document.getElementById('loginForm');
+var userNameArray = [];
+console.log(userNameArray.length);
+
+function saveUserName(event) {
+    var returnPlayer = false;
+    event.preventDefault();
+    var userName = document.getElementById('userName').value;
+    if (localStorage.localUserName){
+        // alert('Local storage exists');
+        userNameArray = JSON.parse(localStorage.getItem('localUserName')); // TODO: Check if username already exists
+        for (var i in userNameArray){
+            if (userName === userNameArray[i]){
+                alert('Welcome back!');
+                returnPlayer = true;
+            }
+        };
+    } else {
+        // alert('if username array is empty');
+    };     
+    if (returnPlayer === false){
+        alert('Good luck!');
+        userNameArray.push(userName);
+    };
+
+    localStorage.setItem('localUserName', JSON.stringify(userNameArray));
+    window.location.href = 'main.html';
 }
 
-// inputForm.addEventListener('submit',saveUserName);
-
-
+inputForm.addEventListener('submit', saveUserName);
